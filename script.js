463248bbbdb50d64cf466c9acbf8c71569bea7c9
John Doe <johndoe@example.com>
2026-06-02 20:00:13 +0300

    clicker

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(button) {
+    button.innerText = "Logout";
+}
+
+function Like(button) {
+    alert("Ninja was liked");
+}
+
+function remove(button) {
+    button.remove();
+}
